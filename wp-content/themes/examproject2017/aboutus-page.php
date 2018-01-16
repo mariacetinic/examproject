11048af37d4334b15bf15200e1def2875ab4bfb5
https://github.com/mariacetinic/html5.git <maria.cetinic@gmail.com>
2018-01-17 00:11:04 +0100

    styling about us page

--- wp-content/themes/examproject2017/aboutus-page.php
+++ wp-content/themes/examproject2017/aboutus-page.php
@@ -1,7 +1,65 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: maria
- * Date: 2018-01-16
- * Time: 23:38
+ * Template Name: About us
  */
+
+get_header();
+
+?>
+    <div class="container-skinny">
+
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    </div>
+
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <?php
+
+                $image = get_field('hero');
+
+                if( !empty($image) ): ?>
+
+                    <section class="hero-images"><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" /></section>
+
+                <?php endif; ?>
+                <div class="container-skinny">
+                <section>
+                    <article>
+                        <?php the_title(); ?>
+                    </article>
+                    <article>
+                        <?php
+
+                        if( have_rows('menu_links') ):
+
+                            while( have_rows('menu_links') ) : the_row();
+
+                                $link = get_sub_field('link');
+
+                                if( $link ): ?>
+
+                                    <a class="button" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
+
+                                <?php endif;
+
+                            endwhile;
+
+                        endif;
+
+                        ?>
+                    </article>
+                </section>
+
+                <section>
+                    <?php the_content(); ?>
+                </section>
+            </article>
+
+        <?php endwhile; endif; ?>
+
+
+</div>
+
+<?php
+get_footer();
+
